1e5d10d82f0a0eedc3feffee7afdcd7a31859631
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2022-11-02 16:09:45 +0000

    First commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/',
+  xRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  xRapidApiHostHeaderValue:'open-weather13.p.rapidapi.com',
+  xRapidApiKeyHeaderName:'X-RapidAPI-Key',
+  xRapidApiKeyHeaderValue: 'a74b413e71msh62e2a44749557a4p19319djsn52d41b229767'
+
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
